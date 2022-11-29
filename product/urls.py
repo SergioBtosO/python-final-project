47a9daa09bf3bff8a2d361b64b64b8b01bb3ec34
Juan Echeverry <juan6david@gmail.com>
2022-11-28 21:59:53 -0500

    add products urls

---
+++ product/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from product import views
+
+app_name = 'product'
+
+urlpatterns = [
+    path("products/", views.ProductListView.as_view(), name="product-list"),
+    path("product/add/", views.ProductCreateView.as_view(), name="product-add"),
+    path("product/<int:pk>/detail/", views.ProductDetailView.as_view(), name="product-detail"),
+    path("product/<int:pk>/update/", views.ProductUpdateView.as_view(), name="product-update"),
+    path("product/<int:pk>/delete/", views.ProductDeleteView.as_view(), name="product-delete")
+
+]
